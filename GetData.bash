fbc43846891143c128dc10ad6d6e62ad7f95b185
E.W. Peter Jalajas <lists@jalajas.com>
2020-12-04 23:11:28 -0500

    Update GetData.bash

--- GetData.bash
+++ GetData.bash
@@ -5,5 +5,19 @@
 #LICENSE: SPDX Apache-2.0
 #VERSION: 
 
+date
+date --utc
+hostname -f
+pwd
+whoami
+echo
 DATA_FILE_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+echo
 curl --trace-ascii - $DATA_FILE_URL| grep Massachusetts > MA.out
+
+
+#REFERENCE
+: '
+Deleting the contents of '/home/runner/work/covid-death-time-chart-patterns/covid-death-time-chart-patterns'
+
+'
