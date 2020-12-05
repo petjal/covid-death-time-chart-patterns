7edd51636af5f9427a9d19f911aedca899ca43aa
E.W. Peter Jalajas <lists@jalajas.com>
2020-12-04 23:13:12 -0500

    find -iname "*.out"

--- GetData.bash
+++ GetData.bash
@@ -14,6 +14,7 @@ echo
 DATA_FILE_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
 echo
 curl --trace-ascii - $DATA_FILE_URL| grep Massachusetts > MA.out
+find -iname "*.out"
 
 
 #REFERENCE
