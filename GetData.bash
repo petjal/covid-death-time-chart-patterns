a3af916e1bd2cb87d22d714b6188d0cd99608570
E.W. Peter Jalajas <lists@jalajas.com>
2020-12-04 21:29:22 -0500

    Create GetData.bash

---
+++ GetData.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+#https://github.com/petjal/covid-death-time-chart-patterns/new/main
+#AUTHOR: pete@jalajas.com
+#DATE: 2020-12-04
+#LICENSE: SPDX Apache-2.0
+
+curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv
